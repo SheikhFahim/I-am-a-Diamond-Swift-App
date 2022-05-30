767b4b2f6835e4cab51129c46ae3fd24f36a1421
sheikh fahim Uzzaman <sheikhfahimuzzaman@sheikhs-MacBook-Pro.local>
2022-05-30 12:17:13 +0600

    first build

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by sheikh fahim Uzzaman on 29/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
